7d4a710a97e1d44b563deb3390f38cda7aada923
Dorian <dorian.pubs@gmail.com>
2025-05-14 16:04:13 +0200

    main_server

--- terraform/main_server.py
+++ terraform/main_server.py
@@ -50,48 +50,55 @@ class ServerStack(TerraformStack):
         account_id, security_group, subnets, default_vpc = self.infra_base()
         
         launch_template = LaunchTemplate(
-            self, "launch template",
-            image_id=""
-            instance_type=", # le type de l'instance
-            vpc_security_group_ids = [],
-            key_name="",
-            user_data=,
-            tags={"Name":"TP noté"},
-            iam_instance_profile={"name":"LabInstanceProfile"}
-            )
+            self, "launch_template",
+            image_id="ami-053b0d53c279acc90",  # Ubuntu 22.04
+            instance_type="t2.micro",
+            vpc_security_group_ids=[security_group.id],
+            key_name="tp-key", 
+            user_data=user_data,
+            tags={"Name": "TP noté"},
+            iam_instance_profile={"name": "LabInstanceProfile"}
+        )
     
 
         lb = Lb(
             self, "lb",
-            load_balancer_type="",
-            security_groups=[],
-            subnets=
+            load_balancer_type="application",
+            security_groups=[security_group.id],
+            subnets=subnets
         )
 
-        target_group=LbTargetGroup(
+
+        target_group = LbTargetGroup(
             self, "tg_group",
-            port=,
-            protocol="",
-            vpc_id=,
-            target_type=""
+            port=8080,
+            protocol="HTTP",
+            vpc_id=default_vpc.id,
+            target_type="instance"
         )
 
         lb_listener = LbListener(
             self, "lb_listener",
-            load_balancer_arn=,
-            port=,
-            protocol="",
-            default_action=[LbListenerDefaultAction()]
+            load_balancer_arn=lb.arn,
+            port=80,
+            protocol="HTTP",
+            default_action=[LbListenerDefaultAction(
+                type="forward",
+                target_group_arn=target_group.arn
+            )]
         )
 
         asg = AutoscalingGroup(
-            self, "",
-            min_size=,
-            max_size=,
-            desired_capacity=,
-            launch_template={"id":},
-            vpc_zone_identifier= ,
-            target_group_arns=[]
+            self, "asg",
+            min_size=1,
+            max_size=2,
+            desired_capacity=1,
+            launch_template={
+                "id": launch_template.id,
+                "version": "$Latest"
+            },
+            vpc_zone_identifier=subnets,
+            target_group_arns=[target_group.arn]
         )
 
     def infra_base(self):
